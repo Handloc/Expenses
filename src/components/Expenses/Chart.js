ce011f01977c3d31ef5678d244eec8545520fed6
Maciej Handke <maciejhandke11@gmail.com>
2023-03-12 22:22:05 +0100

    Add styling for filled bar, add calculating filling height

--- src/components/Expenses/Chart.js
+++ src/components/Expenses/Chart.js
@@ -23,22 +23,40 @@ const BarWrapper = styled.div`
 const ChartBar = styled.div`
   width: 1rem;
   height: 8rem;
-  background-color: #e96900;
-  border-radius: 1rem;
 
+  background-color: #5f5f5f;
+  border-radius: 1rem;
   box-shadow: 0 0 10px black;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  overflow: hidden;
+`;
+
+const CharBarFill = styled.div`
+  width: 100%;
+  background-color: #e96900;
+  transition: all 0.35s ease-out;
 `;
 
 function Chart(props) {
+  let fillHeight = "0%";
+
+  if (props.totalVal > 0) {
+    fillHeight = Math.round((props.value / props.totalVal) * 100) + "%";
+  }
+
   function changeMonth() {
-    props.onMonthChange(props.monthName);
+    props.onMonthChange(props.label);
   }
 
   return (
     <CharBarWrapper>
       <BarWrapper onClick={changeMonth}>
-        <ChartBar />
-        <p>{props.monthName.slice(0, 3)}</p>
+        <ChartBar>
+          <CharBarFill style={{ height: fillHeight }} />
+        </ChartBar>
+        <p>{props.label.slice(0, 3)}</p>
       </BarWrapper>
     </CharBarWrapper>
   );
